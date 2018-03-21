87d6ec302838fa830e6acf46bb9fc7e22d64f1e7
peterdouglasjones <peterdouglasjones@gmail.com>
2018-03-21 16:31:43 -0700

    Fix for Apple computers
    Devante tested working on his Macbook, tweaks remain when testing allows.

--- beamplot.m
+++ beamplot.m
@@ -315,7 +315,7 @@ classdef beamplot < matlab.apps.AppBase
                 if ispc
                     parentfolder = pathname(1:idcs(end-1));
                     %add a check for ismac when devante is here and I can test on his machine
-                    %see if just pathname works
+                    %see if just 'pathname' works on a mac
                 end
             else
                 openparent = strcat(parentfolder,'*.*');
